1cf67a461ce0101915d0bce3a75e78b0f40c4c15
Naexon <a.woda@outlook.com>
2023-06-02 23:38:36 +0200

    formatting

--- src/build_parser.rs
+++ src/build_parser.rs
@@ -7,7 +7,12 @@ use std::path::Path;
 use kube::CustomResource;
 
 #[derive(CustomResource, Clone, Debug, Deserialize, Serialize, JsonSchema)]
-#[kube(group = "task.execd.at", version = "v1alpha1", kind = "Build", namespaced)]
+#[kube(
+    group = "task.execd.at",
+    version = "v1alpha1",
+    kind = "Build",
+    namespaced
+)]
 pub struct BuildSpec {
     baseimage: String,
     description: String,
@@ -45,8 +50,8 @@ pub fn parse_build(file_path: &str) -> BuildSpec {
         .expect("Failed to read file");
 
     // Parse the YAML contents into the build spec structure
-    let build_spec: BuildSpec = serde_yaml::from_str(&file_contents)
-        .expect("Failed to parse devfile");
+    let build_spec: BuildSpec =
+        serde_yaml::from_str(&file_contents).expect("Failed to parse devfile");
 
     build_spec
 }
--- src/main.rs
+++ src/main.rs
@@ -2,15 +2,19 @@ use tracing::log::info;
 
 use clap::Parser;
 
-use kube::{Client, api::{Api, PatchParams, Patch}, CustomResourceExt, runtime::{wait::await_condition, conditions}};
 use k8s_openapi::apiextensions_apiserver::pkg::apis::apiextensions::v1::CustomResourceDefinition;
+use kube::{
+    api::{Api, Patch, PatchParams},
+    runtime::{conditions, wait::await_condition},
+    Client, CustomResourceExt,
+};
 
 mod cli;
 use cli::{Arguments, SubCommands};
 
 mod build_parser;
-use build_parser::parse_build;
 use crate::build_parser::Build;
+use build_parser::parse_build;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
@@ -23,30 +27,38 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
             dbg!(run_args);
             if let Some(yaml_path) = &run_args.input_file {
                 if let Some(path_str) = yaml_path.to_str() {
-                        let d: build_parser::BuildSpec = parse_build(path_str);
+                    let d: build_parser::BuildSpec = parse_build(path_str);
 
-                        let ssapply = PatchParams::apply("execdat-cli").force();
+                    let ssapply = PatchParams::apply("execdat-cli").force();
 
-                        // see https://github.com/kube-rs/kube/blob/main/examples/crd_apply.rs
-                        
-                        // 0. Ensure the CRD is installed (you probably just want to do this on CI)
-                        // (crd file can be created by piping `Foo::crd`'s yaml ser to kubectl apply)
-                        let crds: Api<CustomResourceDefinition> = Api::all(client.clone());
-                        info!("Creating crd: {}", serde_yaml::to_string(&Build::crd())?);
-                        crds.patch("builds.task.execd.at", &ssapply, &Patch::Apply(Build::crd()))
-                            .await?;
+                    // see https://github.com/kube-rs/kube/blob/main/examples/crd_apply.rs
 
-                        info!("Waiting for the api-server to accept the CRD");
-                        let establish = await_condition(crds, "builds.task.execd.at", conditions::is_crd_established());
-                        let _ = tokio::time::timeout(std::time::Duration::from_secs(10), establish).await?;
+                    // 0. Ensure the CRD is installed (you probably just want to do this on CI)
+                    // (crd file can be created by piping `Foo::crd`'s yaml ser to kubectl apply)
+                    let crds: Api<CustomResourceDefinition> = Api::all(client.clone());
+                    info!("Creating crd: {}", serde_yaml::to_string(&Build::crd())?);
+                    crds.patch(
+                        "builds.task.execd.at",
+                        &ssapply,
+                        &Patch::Apply(Build::crd()),
+                    )
+                    .await?;
 
-                        let builds: Api<Build> = Api::default_namespaced(client.clone());
+                    info!("Waiting for the api-server to accept the CRD");
+                    let establish = await_condition(
+                        crds,
+                        "builds.task.execd.at",
+                        conditions::is_crd_established(),
+                    );
+                    let _ =
+                        tokio::time::timeout(std::time::Duration::from_secs(10), establish).await?;
 
-                        let b = Build::new("test", d);
-                        let o = builds.patch("test", &ssapply, &Patch::Apply(&b)).await?;
+                    let builds: Api<Build> = Api::default_namespaced(client.clone());
 
-                        dbg!(o);
-                        
+                    let b = Build::new("test", d);
+                    let o = builds.patch("test", &ssapply, &Patch::Apply(&b)).await?;
+
+                    dbg!(o);
                 } else {
                     println!("Invalid YAML file path");
                 }
